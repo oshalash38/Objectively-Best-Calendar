f0e49bb18ea0ed318ea470b7f86c0248742878e6
Abhijoy-Mandal <a.mandal@mail.utoronto.ca>
2020-03-01 12:38:09 -0500

    Decoupled User and Event+ JavaDoc

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -3,19 +3,31 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a user of the calendar
+ *
+ * @author Abhijoy Mandal
+ */
 public class User implements Serializable{
     private ArrayList<Event> events = new ArrayList<>();
 
-    public void createEvent (String name, Timing time, String sname){
-        sortEvents(new Event(name, time, sname));
+    /** adds an event to this.events
+     *
+     * @param newEvent the name of the event
+     */
+    public void addEvent (Event newEvent){
+        sortEvents(newEvent);
     }
 
-    public void createEvent (String name, Timing time){
-        sortEvents(new Event(name, time));
-    }
-
+    /** returns ArrayList of events in User
+     * @return this.events
+     */
     public ArrayList<Event> getEvents(){return this.events;}
 
+    /** Adds a new event in the sorted Arraylist this.events, and maintains the order.
+     *
+     * @param newEvent the event to be added to the ArrayList
+     */
     private void sortEvents (Event newEvent){
         for (Event e : this.events){
             if (e.compareTo(newEvent) >= 0){
@@ -26,6 +38,11 @@ public class User implements Serializable{
         this.events.add(newEvent);
     }
 
+    /** searches an event in this.events by name
+     *
+     * @param name the name of the event
+     * @return the event if it exists, else null.
+     */
     public Event searchEventByName(String name){
         for (Event e: this.events){
             if (e.getEventName().equals(name)){return e;}
