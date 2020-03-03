ada1bf782af97ec95c6963f06490ee6cef28720e
Peter-Sellars <p.sellars@rogers.com>
2020-03-03 14:02:13 -0500

    changing return values

--- phase1/src/Controller.java
+++ phase1/src/Controller.java
@@ -3,4 +3,10 @@ public class Controller {
      * Class representing the controller for the program
      * @author Omar Shalash
      */
+    private User curr;
+
+    public String createSeriesFromScratch(String seriesName, int duration, String date, int fSelection, int neSelection){
+        //interpret date as localtime and then use between to find elapsed time between localtime and midnight, convert to duration
+        return "";
+    }
 }
