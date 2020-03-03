62890d912855552fe52807958ea42d18fe38d912
shoiche7 <d.shoichet@mail.utoronto.ca>
2020-03-02 20:31:12 -0500

    Created menu options for ViewModel

---
+++ phase1/src/ViewModel.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class displays information to the user.
+ *
+ * @author Daniel Shoichet
+ *
+ */
+
+public class ViewModel {
+    private final String enterOption= "Enter choice:";
+    private final String[] introPage = {"CALENDAR V1", "===============", "1. Login", "2. Create new user", enterOption};
+    private final String[] createUser = {"Enter new username:", "Enter new password:"};
+    private final String[] signIn = {"Enter username:", "Enter password:"};
+    private final String[] mainMenu = {"Main Menu:", "Current alerts:", "1. Check upcoming alerts", "2. Create memo",
+    "3. Create event", "4. Create series", "5. Event options", "6. Display events filtered by...", "7. Logout" , enterOption};
+    private final String[] eventOptions = {"1. Delete event", "2. Associate event with memo",
+            "3. Create alert for event", "4. View alerts for event", "5. Create tag for event", "6. return",
+            enterOption};
+    private final String[] eventInfo = {"Event name:", "Event time:", "Event tag:", "Event memos:", "Event reminders:"};
+    private final String[] displayEventBy = {"Display events filtered by...", "1. Current events", "2. Past events",
+            "3. Future events", "4. Date threshold", "5. Memo", "6. Tag", "7. Return", enterOption};
+
+
+    /**
+     * Creates an instance of ViewModel
+     */
+    public ViewModel(){}
+
+
+
+
+}
