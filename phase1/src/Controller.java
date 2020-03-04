1add03e280d7b0efc2987d22eebbb173ffc756d9
Peter-Sellars <p.sellars@rogers.com>
2020-03-04 17:47:06 -0500

    added functionality and i/o stuff for creating a series from scratch

--- phase1/src/Controller.java
+++ phase1/src/Controller.java
@@ -1,18 +1,22 @@
 import java.time.LocalTime;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Controller {
     /**
      * Class representing the controller for the program
+     *
      * @author Omar Shalash
      */
     private User curr;
     private SeriesManager sm;
+    private Presenter presenter;
 
 
-    public String createSeriesFromScratch(String seriesName, String duration, String date, int fSelection, int neSelection){
+    public String createSeriesFromScratch(String seriesName, String duration, String date, int fSelection, int neSelection) {
         //interpret date as localtime and then use between to find elapsed time between localtime and midnight, convert to duration
-        if (neSelection < 2){
+        if (neSelection < 2) {
             return "Series must have at least 2 events.";
         }
         LocalTime lt = LocalTime.parse(duration);
@@ -21,8 +25,35 @@ public class Controller {
         sm.createSeries(curr, seriesName, d, ldt, fSelection, neSelection);
         return "Series " + seriesName + " created.";
     }
-    public String createSeriesFromEvents(String seriesName){
 
-        return "";
+    public String createSeriesFromEvents(User u, String seriesName) {
+        String s;
+        String[] sSplit;
+        ArrayList<Integer> indices;
+        int i;
+        boolean flawless;
+        do {
+            s = presenter.getEventsForSeries(u);
+            sSplit = s.split(",");
+            indices = new ArrayList<>();
+            flawless = true;
+            for (String selection : sSplit) {
+                try {
+                    i = Integer.parseInt(selection);
+                    if (i < 0 || i > u.getEvents().size() - 1) {
+                        flawless = false;
+                        System.out.println(selection + " is out of bounds. Enter your selections again.");
+                        break;
+                    }
+                    indices.add(i);
+                } catch (NumberFormatException x) {
+                    flawless = false;
+                    System.out.println(selection + " is not a valid input type. Enter your selections again.");
+                    break;
+                }
+            }
+        } while (!flawless);
+        sm.createSeries(seriesName, u, indices);
+        return "Series " + seriesName + " created.";
     }
 }
