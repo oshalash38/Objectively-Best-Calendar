493b4a13e5b536266a8c0efb6577b613d2587608
Peter-Sellars <p.sellars@rogers.com>
2020-03-05 18:47:41 -0500

    added CreateSeriesScratchView, CreateSeriesEventsView

---
+++ phase1/src/views/CreateSeriesEventsView.java
@@ -0,0 +1,13 @@
+package views;
+
+import java.util.List;
+
+/**
+ * The UI for selecting events to put in a new series
+ */
+public class CreateSeriesEventsView extends CalendarView {
+    @Override
+    public List<String> activateView(List<String> outputs) {
+        return null;
+    }
+}
---
+++ phase1/src/views/CreateSeriesScratchView.java
@@ -0,0 +1,14 @@
+package views;
+
+import java.util.List;
+
+/**
+ * The UI for creating a series from scratch
+ * (i.e. when the user enters frequency, duration, number of events, series name)
+ */
+public class CreateSeriesScratchView extends CalendarView{
+    @Override
+    public List<String> activateView(List<String> outputs) {
+        return null;
+    }
+}
