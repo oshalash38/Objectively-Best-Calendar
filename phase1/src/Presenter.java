1f88d5f20c4ca11d40f5e65d47d1ee1a2ae9f268
Abhijoy-Mandal <a.mandal@mail.utoronto.ca>
2020-03-04 20:40:49 -0500

    Possibly the new version

--- phase1/src/Presenter.java
+++ phase1/src/Presenter.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 /**
@@ -6,7 +11,8 @@ import java.util.Scanner;
  * @author Daniel Shoichet
  */
 public class Presenter {
-    private Scanner kbReader = new Scanner(System.in);
+    private ViewModel myModel = new ViewModel();
+    private MenuManger myMenus = new MenuManger();
 
     /**
      * Instantiates a Presenter class
@@ -16,10 +22,78 @@ public class Presenter {
 
     /**
      * Display the startup page
-     * TODO make safeguards so it doesn't crash when user enters something other than 1 or 2
+     *
+     * #TODO THIS METHOD HAS DUPLICATE CODE NEED TO FIND WAY TO REMOVE IT
+     *
      * @return 1 if the user selected to login or 2 if the user selected to create a new account
      */
+    public int menuSelection(Menu menu){
+        int input = 0;
+        myModel.displayOptionsMenu(menu.getContents());
+        try {
+            Scanner kbReader = new Scanner(System.in);
+            input = kbReader.nextInt();
+        }catch (InputMismatchException e){
+            myModel.pleaseTryAgain();
+            return menuSelection(menu);
+        }
+        if(!((1 <= input) && (input <= menu.getNumOptions()))){
+            myModel.pleaseTryAgain();
+            return menuSelection(menu);
+        }
+        return input;
+    }
+
+    public int getStartUpPageInput(){
+        return menuSelection(myMenus.getStartUpPage());
+    }
+
+
 //    public int startupPage(){
 //
 //    }
+    private String displayTags(Event e){
+        StringBuilder s = new StringBuilder();
+        for (String tag: e.getTags()){
+            s.append(tag);
+            s.append(", ");
+        }
+        if (s.toString().equals("")){ return "";}
+        return s.substring(0, s.length()-2);
+    }
+
+    private String displayMemoIds(Event e){
+        StringBuilder s = new StringBuilder();
+        for (int id: e.getMemoIDs()){
+            s.append(id);
+            s.append(", ");
+        }
+        if (s.toString().equals("")){ return "";}
+        return s.substring(0, s.length()-2);
+    }
+    private String displayAlerts(Event e){
+        StringBuilder s = new StringBuilder();
+        for (Alert a: e.getAlerts()){
+            s.append(a.pushReminder());
+            s.append(", ");
+        }
+        if (s.toString().equals("")){ return "";}
+        return s.substring(0, s.length()-2);
+    }
+
+    public String getEventsForSeries(User u){
+        String[] s;
+        int counter = 0;
+        for (Event e: u.getEvents()){
+            s = new String[]{e.getEventName(), e.getTime().getStart().toString(),
+                    e.getTime().getEnd().toString(), e.getSeriesName(), displayTags(e),
+            displayMemoIds(e), displayAlerts(e)};
+            myModel.displayEventInfo(s, counter);
+            counter ++;
+        }
+        myModel.displaySelectionEventsForSeries();
+        Scanner kbReader = new Scanner(System.in);
+        return kbReader.next();
+    }
+
 }
