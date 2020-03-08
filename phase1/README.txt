489e8625c7b97248739f0b7581573ddcccbe8127
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-03-08 14:04:57 -0400

    Read me basics and input verification for create date time

---
+++ phase1/README.txt
@@ -0,0 +1,12 @@
+To start the program, if you are using intellij then run the set up the program so that Main.java the class you run.
+
+You may create a new user if you desire or use these credentials
+Username:
+
+Password:
+
+to access an account with premade events and alerts.
+
+
+Instructions:
+In menus if you are given multiple options that are numbered you are required to input the number corresponding to your desired option.
--- phase1/src/views/CreateDateTimeView.java
+++ phase1/src/views/CreateDateTimeView.java
@@ -16,16 +16,40 @@ public class CreateDateTimeView extends CalendarView {
     @Override
     public List<String> activateView(List<String> outputs) {
         List<String> inputs = new ArrayList<>();
-        System.out.println("Enter year:");
-        inputs.add(in.nextLine());
-        System.out.println("Enter month number: (1-12)");
-        inputs.add(in.nextLine());
-        System.out.println("Enter day of month: (1-31)");
-        inputs.add(in.nextLine());
-        System.out.println("Enter hour: (24 hour clock)");
-        inputs.add(in.nextLine());
-        System.out.println("Enter minute: (0-59)");
-        inputs.add(in.nextLine());
+
+        try{
+            System.out.println("Enter year:");
+            inputs.add(in.nextLine());
+            Integer.parseInt(inputs.get(0));
+
+            System.out.println("Enter month number: (1-12)");
+            inputs.add(in.nextLine());
+            validInteger(inputs.get(1), 1, 12);
+
+            System.out.println("Enter day of month: (1-31)");
+            inputs.add(in.nextLine());
+            validInteger(inputs.get(2), 1, 31);
+
+            System.out.println("Enter hour: (24 hour clock)");
+            inputs.add(in.nextLine());
+            validInteger(inputs.get(3), 0, 23);
+
+            System.out.println("Enter minute: (0-59)");
+            inputs.add(in.nextLine());
+            validInteger(inputs.get(4), 0, 59);
+
+        } catch (Exception e) {
+            System.out.println("\n======INVALID INPUT! PLEASE RETRY!=====\n\n");
+            return activateView(outputs);
+        }
+
         return inputs;
+    }
+
+    public void validInteger(String input, int min, int max) throws Exception {
+        int num = Integer.parseInt(input);
+
+        if(num < min || num > max)
+            throw new Exception();
     }
 }
