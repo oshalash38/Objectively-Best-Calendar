ba408fd746baa701ea89baaa904549af447512b7
Peter-Sellars <p.sellars@rogers.com>
2020-03-18 16:28:06 -0400

    the first tests!

---
+++ phase2/src/com/group_0225/tests/com/group_0225/SeriesManagerTest.java
@@ -0,0 +1,16 @@
+package com.group_0225;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeriesManagerTest {
+
+    @Test
+    void createSeries() {
+    }
+
+    @Test
+    void testCreateSeries() {
+    }
+}
