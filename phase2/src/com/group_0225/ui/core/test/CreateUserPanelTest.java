242fd6392516b32e1806756418854bbb5986232a
shoiche7 <d.shoichet@mail.utoronto.ca>
2020-03-18 15:27:45 -0500

    Added 1 test for the GUI

---
+++ phase2/src/com/group_0225/ui/core/test/CreateUserPanelTest.java
@@ -0,0 +1,24 @@
+package com.group_0225.ui.core.test;
+
+import com.group_0225.ui.core.CalendarPanel;
+import com.group_0225.ui.core.CalendarToolBar;
+import com.group_0225.ui.core.CreateUserPanel;
+import com.group_0225.ui.core.StartupPanel;
+
+import javax.swing.*;
+
+class CreateUserPanelTest extends JFrame {
+    public static void main(String[] args){
+        CreateUserPanelTest c = new CreateUserPanelTest();
+        c.run();
+    }
+
+    private void run(){
+
+        this.add(new StartupPanel());
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        this.setJMenuBar(new CalendarToolBar());
+    }
+}
