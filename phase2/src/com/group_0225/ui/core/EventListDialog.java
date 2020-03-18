5b1841032fb38fd3d21c85c01ee3aed1f3efe663
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-03-18 18:18:19 -0400

    Show event list base

--- phase2/src/com/group_0225/ui/core/EventListDialog.java
+++ phase2/src/com/group_0225/ui/core/EventListDialog.java
@@ -1,13 +1,31 @@
 package com.group_0225.ui.core;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class EventListDialog extends JDialog {
 
     public EventListDialog() {
         super();
+        this.setSize(400, 400);
+        this.setLocation(300, 200);
 
-        JPanel eventList = new JPanel();
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weighty = 1;
+        c.weightx = 1;
+
+        JPanel parent = new JPanel(new GridBagLayout());
+        parent.setBackground(Color.YELLOW);
+
+        for (int i = 0; i < 30; i++) {
+            c.gridy = i;
+            JButton eventPreview = new JButton("Event " + i);
+            parent.add(eventPreview, c);
+        }
+
+        JScrollPane panel = new JScrollPane(parent);
+        this.add(panel);
     }
 
 }
