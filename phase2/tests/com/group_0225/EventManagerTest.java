03b8c6cf14e2bed787bb40bd61e0a0bec3b659ad
Peter-Sellars <p.sellars@rogers.com>
2020-03-29 01:04:44 -0400

    empty test file for EventManager

---
+++ phase2/tests/com/group_0225/EventManagerTest.java
@@ -0,0 +1,48 @@
+package com.group_0225;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventManagerTest {
+
+    @Test
+    public void getUpcomingEvents() {
+    }
+
+    @Test
+    public void getCurrentEvents() {
+    }
+
+    @Test
+    public void getPastEvents() {
+    }
+
+    @Test
+    public void formatEventByName() {
+    }
+
+    @Test
+    public void getEventsByTag() {
+    }
+
+    @Test
+    public void getEventsBySeriesName() {
+    }
+
+    @Test
+    public void getDetailedEvent() {
+    }
+
+    @Test
+    public void getEventsBetween() {
+    }
+
+    @Test
+    public void addTag() {
+    }
+
+    @Test
+    public void deleteEvent() {
+    }
+}
